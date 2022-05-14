f55d4aa2591714fc29641a4de11802a0f751dba2
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-05-14 21:08:35 +0530

    Update keys.js

--- keys/keys.js
+++ keys/keys.js
@@ -6,3 +6,90 @@ let keyy="WVsgYYQvoBfkb01o"
 
 module.exports= { MONGO_DATABASE,MONGO_PASS,MONGO_USER, Jwt} 
 
+
+
+// app.css
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+}
+
+.active {
+  color: aqua;
+}
+
+.margin_top-10 {
+  margin-top: 10px;
+}
+
+.margin_top-15 {
+  margin-top: 15px;
+}
+
+.margin_top-20 {
+  margin-top: 20px;
+}
+
+.margin_bottom-10 {
+  margin-bottom: 10px;
+}
+
+.margin_bottom-15 {
+  margin-bottom: 15px;
+}
+
+.margin_bottom-20 {
+  margin-bottom: 20px;
+}
+
+/* padding */
+.padding-10 {
+  padding: 10px;
+}
+
+.padding-20 {
+  padding: 20px 0px;
+}
+.padding-30 {
+  padding: 30px 0px;
+}
+
+.center {
+  text-align: center;
+}
+
+/* button */
+button {
+  border: none;
+  outline: none;
+  box-shadow: none;
+}
+
+/* flex */
+.posi-rel {
+  position: relative;
+}
+.d-f {
+  display: flex;
+}
+.d-f-center {
+  justify-content: center;
+  align-items: center;
+}
+
+.jc-sb {
+  justify-content: space-between;
+  align-items: center;
+}
+
+.jc-sa {
+  justify-content: space-around;
+  align-items: center;
+}
+
