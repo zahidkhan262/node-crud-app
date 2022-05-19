c8c8e88815f396deecc0f03016ce0c85bb93ff7a
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-05-19 23:20:09 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -171,4 +171,62 @@ router.post('/signin', async (req, res) => {
 
 })
 
+
+
+// new++++
+
+
+router.post('/reset-password',(req,res)=>{
+     crypto.randomBytes(32,(err,buffer)=>{
+         if(err){
+             console.log(err)
+         }
+         const token = buffer.toString("hex")
+         User.findOne({email:req.body.email})
+         .then(user=>{
+             if(!user){
+                 return res.status(422).json({error:"User dont exists with that email"})
+             }
+             user.resetToken = token
+             user.expireToken = Date.now() + 3600000
+             user.save().then((result)=>{
+                 transporter.sendMail({
+                     to:user.email,
+                     from:"no-replay@insta.com",
+                     subject:"password reset",
+                     html:`
+                     <p>You requested for password reset</p>
+                     <h5>click in this <a href="${EMAIL}/reset/${token}">link</a> to reset password</h5>
+                     `
+                 })
+                 res.json({message:"check your email"})
+             })
+
+         })
+     })
+})
+
+
+router.post('/new-password',(req,res)=>{
+    const newPassword = req.body.password
+    const sentToken = req.body.token
+    User.findOne({resetToken:sentToken,expireToken:{$gt:Date.now()}})
+    .then(user=>{
+        if(!user){
+            return res.status(422).json({error:"Try again session expired"})
+        }
+        bcrypt.hash(newPassword,12).then(hashedpassword=>{
+           user.password = hashedpassword
+           user.resetToken = undefined
+           user.expireToken = undefined
+           user.save().then((saveduser)=>{
+               res.json({message:"password updated success"})
+           })
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+
 module.exports = router
