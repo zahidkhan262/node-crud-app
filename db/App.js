24af9735a240eaf18f61bc511b1ffdf1c71094d2
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-05-26 23:40:05 +0530

    Update App.js

--- db/App.js
+++ db/App.js
@@ -18,5 +18,16 @@ const App = () => {
     </>
   )
 }
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+export default function Loading() {
+    return (
+        <Box sx={{ width: '100%' }}>
+            <LinearProgress />
+        </Box>
+    );
+}
 
 export default App
